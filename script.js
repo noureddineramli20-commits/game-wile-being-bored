f1acfd7d539779c16c0106e0034ae9024b5ba1be
noureddine ramli <noureddine.ramli2.0@gmail.com>
2026-07-25 21:04:22 +0200

    Implement score feedback messages
    Added conditional messages for various score thresholds to provide feedback to the user based on their score.

--- script.js
+++ script.js
@@ -14,8 +14,308 @@ document.getElementById("coin2").onclick = function () {
 
 document.getElementById("coin3").onclick = function () {
   something += 100;
-  document.getElementById("score").textContent = something;
 };
 
+console.log(something);
 
-console.log("something");
+if (something >= 100000) {
+  document.getElementById("concrats").textContent =
+    "concrats you got the finel point 100.000 point thank you for playing";
+} else if (something >= 99000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 99.000 points need 1.000 more points!";
+} else if (something >= 98000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 98.000 points need 2.000 more points!";
+} else if (something >= 97000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 97.000 points need 3.000 more points!";
+} else if (something >= 96000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 96.000 points need 4.000 more points!";
+} else if (something >= 95000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 95.000 points need 5.000 more points!";
+} else if (something >= 94000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 94.000 points need 6.000 more points!";
+} else if (something >= 93000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 93.000 points need 7.000 more points!";
+} else if (something >= 92000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 92.000 points need 8.000 more points!";
+} else if (something >= 91000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 91.000 points need 9.000 more points!";
+} else if (something >= 90000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 90.000 points need 10.000 more points!";
+} else if (something >= 89000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 89.000 points need 11.000 more points!";
+} else if (something >= 88000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 88.000 points need 12.000 more points!";
+} else if (something >= 87000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 87.000 points need 13.000 more points!";
+} else if (something >= 86000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 86.000 points need 14.000 more points!";
+} else if (something >= 85000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 85.000 points need 15.000 more points!";
+} else if (something >= 84000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 84.000 points need 16.000 more points!";
+} else if (something >= 83000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 83.000 points need 17.000 more points!";
+} else if (something >= 82000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 82.000 points need 18.000 more points!";
+} else if (something >= 81000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 81.000 points need 19.000 more points!";
+} else if (something >= 80000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 80.000 points need 20.000 more points!";
+} else if (something >= 79000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 79.000 points need 21.000 more points!";
+} else if (something >= 78000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 78.000 points need 22.000 more points!";
+} else if (something >= 77000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 77.000 points need 23.000 more points!";
+} else if (something >= 76000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 76.000 points need 24.000 more points!";
+} else if (something >= 75000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 75.000 points need 25.000 more points!";
+} else if (something >= 74000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 74.000 points need 26.000 more points!";
+} else if (something >= 73000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 73.000 points need 27.000 more points!";
+} else if (something >= 72000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 72.000 points need 28.000 more points!";
+} else if (something >= 71000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 71.000 points need 29.000 more points!";
+} else if (something >= 70000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 70.000 points need 30.000 more points!";
+} else if (something >= 69000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 69.000 points need 31.000 more points!";
+} else if (something >= 68000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 68.000 points need 32.000 more points!";
+} else if (something >= 67000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 67.000 points need 33.000 more points!";
+} else if (something >= 66000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 66.000 points need 34.000 more points!";
+} else if (something >= 65000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 65.000 points need 35.000 more points!";
+} else if (something >= 64000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 64.000 points need 36.000 more points!";
+} else if (something >= 63000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 63.000 points need 37.000 more points!";
+} else if (something >= 62000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 62.000 points need 38.000 more points!";
+} else if (something >= 61000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 61.000 points need 39.000 more points!";
+} else if (something >= 60000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 60.000 points need 40.000 more points!";
+} else if (something >= 59000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 59.000 points need 41.000 more points!";
+} else if (something >= 58000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 58.000 points need 42.000 more points!";
+} else if (something >= 57000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 57.000 points need 43.000 more points!";
+} else if (something >= 56000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 56.000 points need 44.000 more points!";
+} else if (something >= 55000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 55.000 points need 45.000 more points!";
+} else if (something >= 54000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 54.000 points need 46.000 more points!";
+} else if (something >= 53000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 53.000 points need 47.000 more points!";
+} else if (something >= 52000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 52.000 points need 48.000 more points!";
+} else if (something >= 51000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 51.000 points need 49.000 more points!";
+} else if (something >= 50000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 50.000 points need 50.000 more points!";
+} else if (something >= 49000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 49.000 points need 51.000 more points!";
+} else if (something >= 48000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 48.000 points need 52.000 more points!";
+} else if (something >= 47000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 47.000 points need 53.000 more points!";
+} else if (something >= 46000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 46.000 points need 54.000 more points!";
+} else if (something >= 45000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 45.000 points need 55.000 more points!";
+} else if (something >= 44000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 44.000 points need 56.000 more points!";
+} else if (something >= 43000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 43.000 points need 57.000 more points!";
+} else if (something >= 42000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 42.000 points need 58.000 more points!";
+} else if (something >= 41000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 41.000 points need 59.000 more points!";
+} else if (something >= 40000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 40.000 points need 60.000 more points!";
+} else if (something >= 39000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 39.000 points need 61.000 more points!";
+} else if (something >= 38000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 38.000 points need 62.000 more points!";
+} else if (something >= 37000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 37.000 points need 63.000 more points!";
+} else if (something >= 36000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 36.000 points need 64.000 more points!";
+} else if (something >= 35000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 35.000 points need 65.000 more points!";
+} else if (something >= 34000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 34.000 points need 66.000 more points!";
+} else if (something >= 33000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 33.000 points need 67.000 more points!";
+} else if (something >= 32000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 32.000 points need 68.000 more points!";
+} else if (something >= 31000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 31.000 points need 69.000 more points!";
+} else if (something >= 30000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 30.000 points need 70.000 more points!";
+} else if (something >= 29000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 29.000 points need 71.000 more points!";
+} else if (something >= 28000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 28.000 points need 72.000 more points!";
+} else if (something >= 27000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 27.000 points need 73.000 more points!";
+} else if (something >= 26000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 26.000 points need 74.000 more points!";
+} else if (something >= 25000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 25.000 points need 75.000 more points!";
+} else if (something >= 24000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 24.000 points need 76.000 more points!";
+} else if (something >= 23000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 23.000 points need 77.000 more points!";
+} else if (something >= 22000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 22.000 points need 78.000 more points!";
+} else if (something >= 21000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 21.000 points need 79.000 more points!";
+} else if (something >= 20000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 20.000 points need 80.000 more points!";
+} else if (something >= 19000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 19.000 points need 81.000 more points!";
+} else if (something >= 18000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 18.000 points need 82.000 more points!";
+} else if (something >= 17000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 17.000 points need 83.000 more points!";
+} else if (something >= 16000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 16.000 points need 84.000 more points!";
+} else if (something >= 15000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 15.000 points need 85.000 more points!";
+} else if (something >= 14000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 14.000 points need 86.000 more points!";
+} else if (something >= 13000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 13.000 points need 87.000 more points!";
+} else if (something >= 12000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 12.000 points need 88.000 more points!";
+} else if (something >= 11000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 11.000 points need 89.000 more points!";
+} else if (something >= 10000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 10.000 points need 90.000 more points!";
+} else if (something >= 9000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 9.000 points need 91.000 more points!";
+} else if (something >= 8000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 8.000 points need 92.000 more points!";
+} else if (something >= 7000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 7.000 points need 93.000 more points!";
+} else if (something >= 6000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 6.000 points need 94.000 more points!";
+} else if (something >= 5000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 5.000 points need 95.000 more points!";
+} else if (something >= 4000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 4.000 points need 96.000 more points!";
+} else if (something >= 3000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 3.000 points need 97.000 more points!";
+} else if (something >= 2000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 2.000 points need 98.000 more points!";
+} else if (something >= 1000) {
+  document.getElementById("concrats").textContent =
+    "concrats you have got your 1.000 points need 99.000 more points!";
+}
